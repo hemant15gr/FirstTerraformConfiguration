d7b61cc4d95ffc9caa999c3fb804127338dfb53d
Sergey Migalnikov <sergey.migalnikov@ciber.no>
2019-02-01 10:07:25 +0100

    Created

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resourceGroupName" {
+  type        = "string"
+  description = "The name of resource group "
+}
+
+variable "location" {
+  type        = "string"
+  description = "Location "
+}
+
+variable "subscriptionId" {
+  type        = "string"
+  description = "Subscription id"
+}
+
+variable "tenantId" {
+  type        = "string"
+  description = "Tenant id"
+}
+
+variable "clientId" {
+  type        = "string"
+  description = "Client id"
+}
+
+variable "clientSecret" {
+  type        = "string"
+  description = "Client secret"
+}
